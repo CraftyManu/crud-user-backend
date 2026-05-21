8279cd3bbece0aa98231a133f671c0880efc91bb
Manu Sartor <manuelasartor2015@gmail.com>
2026-05-21 19:45:35 -0300

    Schema para los usuarios eliminados

--- src/models/audit.model.js
+++ src/models/audit.model.js
@@ -1,1 +1,19 @@
 // para guardar los datos de los usuarios eliminados
+import { required } from "joi";
+import mongoose from "mongoose";
+
+const auditSchema = new mongoose.Schema({
+    usuarioEliminado: {
+        type: Object,
+        required: true
+    },
+    fechaEliminacion: {
+        type: Date,
+        default: Date.now //hora del servidor
+    }
+})
+
+const Audit = mongoose.model('Audit', auditSchema)
+
+export default Audit
+

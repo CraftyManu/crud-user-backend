6b015392af03f5ec1ea5d14fa343056c2ea3b706
Manu Sartor <manuelasartor2015@gmail.com>
2026-07-10 19:01:35 -0300

    refactor: reorganize imports and enable CORS middleware

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
 import express from 'express'
 import './config/env.js'
-
 import connectDB from './config/db.js'
-
-import { env } from "./config/env.js" 
-
+import corsConfig from './config/cors.js'
+/* import { env } from "./config/env.js"  */
 import userRoutes from './routes/user.routes.js'
 import authRoutes from './routes/auth.routes.js'
 
 const app = express() //levanta el backend como función
+app.use(corsConfig);
 app.use(express.json()) //para poder usar json desde el body
-
 connectDB() //llamar a la base de datos
 
 //llamar a las rutas
